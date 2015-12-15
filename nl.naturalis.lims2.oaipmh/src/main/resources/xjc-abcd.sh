78d0b245ca7e3b0bb8a71815bf96388999bc9adf
Ayco Holleman <ayco.holleman@naturalis.nl>
2015-12-15 16:04:22 +0100

    Generated JAXB classes for ABCD

--- nl.naturalis.lims2.oaipmh/src/main/resources/xjc-abcd.sh
+++ nl.naturalis.lims2.oaipmh/src/main/resources/xjc-abcd.sh
@@ -1,3 +1,3 @@
 xjc -d /home/ayco/git/Lims2-Plugins/nl.naturalis.lims2.oaipmh/src/main/java \
-	-p nl.naturalis.lims2.oaipmh.ggbn \
-	GGBN.XSD 
+	-p nl.naturalis.lims2.oaipmh.abcd \
+	ABCD_2.06b.xsd 
