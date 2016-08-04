9eb03db65d128cab16d20e9f8293f4d3a668a8af
Ayco Holleman <ayco.holleman@naturalis.nl>
2016-08-04 11:11:45 +0200

    create unit test for LimsImporterUtil

---
+++ nl.naturalis.lims2.utils/src/test/java/nl/naturalis/lims2/utils/LimsImporterUtilTest.java
@@ -0,0 +1,54 @@
+package nl.naturalis.lims2.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LimsImporterUtilTest {
+
+	@Test
+	public void testRequired() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLogFilename() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLogPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLimsImportDir() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPropValues() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFileFromPropertieFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDatabasePropValues() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExtractNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsAlpha() {
+		fail("Not yet implemented");
+	}
+
+}
