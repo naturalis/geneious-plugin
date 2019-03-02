2d566512d41847ad39c4a8042ea909d9e29242fd
Ayco Holleman <ayco.holleman@naturalis.nl>
2019-03-02 12:29:32 +0100

    javadoc

--- v2/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"nl.naturalis.geneious.bold"},{"l":"nl.naturalis.geneious.crs"},{"l":"nl.naturalis.geneious.gui"},{"l":"nl.naturalis.geneious.gui.log"},{"l":"nl.naturalis.geneious.note"},{"l":"nl.naturalis.geneious.split"},{"l":"nl.naturalis.geneious.trace"},{"l":"nl.naturalis.geneious.util"}]
