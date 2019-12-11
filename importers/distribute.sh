ee452660ac9c5cf2734168fb06174c6d79bd5a75
Ayco Holleman <ayco.holleman@naturalis.nl>
2019-12-11 16:36:12 +0100

    fixed bug in distribute.sh

--- importers/distribute.sh
+++ importers/distribute.sh
@@ -7,25 +7,26 @@
 #
 # USAGE: distribute.sh [-p|--publish] [git_tag]
 
-# Constants
-naturalis_plugin_version="2.0"
-naturalis_common_version="1.0"
+
+# Don't allow builds from non-master branches
+expected_branch="v2_master"
+# Force users to specify a version (i.e. tag) that starts with this string
+expected_version="V2."
+
+
+# Library versions
 commons_compress_version="1.18"
 xml_beans_version="3.0.2"
 poi_version="4.0.1"
 univocity_version="2.7.6"
-expected_branch="v2_master"
-expected_version="V2."
 
 here=$(pwd)
 
 publish=""
 version=""
 
-for arg in "${@}"
-do
-  if [ "${arg}" = -p -o "${arg}" = --publish ]
-  then
+for arg in "${@}"; do
+  if [[ ${arg} = '-p' || ${arg} = '--publish' ]]; then
     publish=1
   else
   	version="${arg}"
@@ -49,7 +50,7 @@ fi
 
 if [ "${version}" -a "${version:0:3}" != ${expected_version} ]
 then
-  echo "Invalid version: ${version}. Versions must start with ${expected_version}"
+  echo "Invalid version: ${version}. Versions must start with \"${expected_version}\""
   exit 1
 fi
 
@@ -62,22 +63,6 @@ else
   read -p "Do you wish to create a distributable for version ${version}? [Y/n] " answer
   [ "${answer}" = "n" -o "${answer}" = "N" ] && exit 0
 fi
-
-
-echo
-echo
-echo "************************************************************************"
-echo "Building naturalis-common"
-echo "************************************************************************"
-naturalis_common_dir=$(realpath "${here}/../../naturalis-common")
-if [ ! -d "${naturalis_common_dir}" ]
-then
-  echo "Missing directory: ${naturalis_common_dir}"
-  echo "Please clone the naturalis-common git repo first"
-  exit 1
-fi
-cd ${naturalis_common_dir}
-mvn clean install || exit 1
 
 
 cd ${here}
@@ -95,7 +80,6 @@ echo "Building nl.naturalis.geneious"
 echo "************************************************************************"
 mvn clean install || exit 1
 
-
 echo
 echo
 echo "************************************************************************"
@@ -104,37 +88,43 @@ echo "************************************************************************"
 assembly_dir="${here}/target/nl.naturalis.geneious.NaturalisGeneiousPlugin"
 [ -d ${assembly_dir} ] && rm -rf ${assembly_dir}
 mkdir ${assembly_dir}
-mv "${here}/target/naturalis-geneious-plugin-${naturalis_plugin_version}.jar" "${assembly_dir}/"
-[ ${?} != 0 ] && exit 1
-mvn dependency:copy "-Dartifact=nl.naturalis.common:naturalis-common:${naturalis_common_version}" "-DoutputDirectory=${assembly_dir}"
-[ ${?} != 0 ] && exit 1
-mvn dependency:copy "-Dartifact=org.apache.poi:poi:${poi_version}" "-DoutputDirectory=${assembly_dir}"
-[ ${?} != 0 ] && exit 1
-mvn dependency:copy "-Dartifact=org.apache.poi:poi-ooxml:${poi_version}" "-DoutputDirectory=${assembly_dir}"
-[ ${?} != 0 ] && exit 1
-mvn dependency:copy "-Dartifact=org.apache.poi:poi-ooxml-schemas:${poi_version}" "-DoutputDirectory=${assembly_dir}"
-[ ${?} != 0 ] && exit 1
-mvn dependency:copy "-Dartifact=org.apache.commons:commons-compress:${commons_compress_version}" "-DoutputDirectory=${assembly_dir}"
-[ ${?} != 0 ] && exit 1
-mvn dependency:copy "-Dartifact=org.apache.xmlbeans:xmlbeans:${xml_beans_version}" "-DoutputDirectory=${assembly_dir}"
-[ ${?} != 0 ] && exit 1
-mvn dependency:copy "-Dartifact=com.univocity:univocity-parsers:${univocity_version}" "-DoutputDirectory=${assembly_dir}"
+cd "${here}/target"
+
+#Don't quote *.jar !! Will result in error
+mv *.jar "${assembly_dir}/"
 [ ${?} != 0 ] && exit 1
 
+cd "${here}"
+mvn dependency:copy-dependencies "-DexcludeScope=system" "-DoutputDirectory=${assembly_dir}"
+[ ${?} != 0 ] && exit 1
+
+#mvn dependency:copy "-Dartifact=nl.naturalis.common:naturalis-common:${naturalis_common_version}" "-DoutputDirectory=${assembly_dir}"
+#[ ${?} != 0 ] && exit 1
+#mvn dependency:copy "-Dartifact=org.apache.poi:poi:${poi_version}" "-DoutputDirectory=${assembly_dir}"
+#[ ${?} != 0 ] && exit 1
+#mvn dependency:copy "-Dartifact=org.apache.poi:poi-ooxml:${poi_version}" "-DoutputDirectory=${assembly_dir}"
+#[ ${?} != 0 ] && exit 1
+#mvn dependency:copy "-Dartifact=org.apache.poi:poi-ooxml-schemas:${poi_version}" "-DoutputDirectory=${assembly_dir}"
+#[ ${?} != 0 ] && exit 1
+#mvn dependency:copy "-Dartifact=org.apache.commons:commons-compress:${commons_compress_version}" "-DoutputDirectory=${assembly_dir}"
+#[ ${?} != 0 ] && exit 1
+#mvn dependency:copy "-Dartifact=org.apache.xmlbeans:xmlbeans:${xml_beans_version}" "-DoutputDirectory=${assembly_dir}"
+#[ ${?} != 0 ] && exit 1
+#mvn dependency:copy "-Dartifact=com.univocity:univocity-parsers:${univocity_version}" "-DoutputDirectory=${assembly_dir}"
+#[ ${?} != 0 ] && exit 1
 
 echo
 echo
 echo "************************************************************************"
 echo "Creating distributable"
 echo "************************************************************************"
-created="$(date +%Y%m%d.%H%M)"
-name="${curversion}.${created}.gplugin"
+created="$(date +%Y%m%d%H%M)"
+name="${curversion}-${created}.gplugin"
 cd ${here}/target
 zip -r ${name} nl.naturalis.geneious.NaturalisGeneiousPlugin
 [ ${?} != 0 ] && exit 1
 
-if [ ! "${publish}" ]
-then
+if [[ -z ${publish} ]]; then
   echo "Distributable: ${here}/target/${name}"
   exit 0
 fi
@@ -148,8 +138,7 @@ git_repo=$(realpath ${here}/../)
 mv "${here}/target/${name}" "${git_repo}/distributable/${name}"
 
 cd ${git_repo}
-if [ -z ${version} ]
-then
+if [[ -z ${version} ]]; then
 	git add --all && git commit -m "Regenerated distributable for version ${curversion}" && git push
 	[ ${?} != 0 ] && exit 1
 else
